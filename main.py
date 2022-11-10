4b053aca56116fca6877cddea669101f80664f20
Tom Cummings <tom.cummings07@outlook.com>
2022-11-10 20:25:22 +0000

    Setting up the repository
    Create .gitignore:
        - Added venv folder.
    
    Update README.md:
        - Capitalised title
    
    Create main.py:
        - Imported PyGame
        - Initialised PyGame
    
    Added PyGame display:
        - Added display with test dimensions
    
    Infinite loop added:
        - Infinite loop added with event checking for quit event.

---
+++ main.py
@@ -0,0 +1,16 @@
+import pygame
+
+pygame.init()
+
+size = [700,400]
+
+screen = pygame.display.set_mode(size)
+
+run = True
+
+while run:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+pygame.quit()
