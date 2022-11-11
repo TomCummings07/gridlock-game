b6a2c7c1010eb89dbd477c894f4996ded66dd1c4
Tom Cummings <tom.cummings07@outlook.com>
2022-11-11 23:37:30 +0000

    Infinite loop added
    - Infinite loop added with event checking for quit event.

--- main.py
+++ main.py
@@ -5,3 +5,14 @@ pygame.init()
 size = [700,400]
 
 screen = pygame.display.set_mode(size)
+
+run = True
+
+while run:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+    
+
+pygame.quit()
